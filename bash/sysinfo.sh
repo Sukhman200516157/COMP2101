0203dbfbe94d5000b4573de50b514615d7c40124
Sukhman200516157 <124110347+Sukhman200516157@users.noreply.github.com>
2023-02-01 20:05:27 -0500

    Update sysinfo.sh

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-# sysinfo.sh - a script to display information about a computer
+# It a script to display information about a computer
 
-# The purpose of this script is to display some important identity information about a computer so that you can see that information quickly and concisely, without having to enter multiple commands or remember multiple command options.
+# The purpose of this script is to display some important identity information about a computer.
 
-# It is a typical script in that it is intended to be useful, time and labour saving, and easily used by any user of the computer regardless of whether they know or understand any of the commands in your script.
-
+#The system’s fully-qualified domain name (FQDN)
 FQDN=$(hostname)
 
 # The operating system name and version
 
-OS_NAME=$(hostnamectl | grep "Operating System" | cut -d " " -f5-)
+OS_NAME=$(hostnamectl | grep "Operating System" )
 
 # Any IP addresses the machine has that are not on the 127 network 
 
@@ -24,10 +23,10 @@ ROOT_SPACE=$(df -h | grep "/$" | tr -s " " | cut -d " " -f4)
 
 echo "Fully Qualified Domain Name: $FQDN"
 
-echo "Operating System: $OS_NAME"
+echo "Host Information: $OS_NAME"
 
 echo "IP Addresses: $IP_ADDRESSES"
 
 echo "Root Filesystem Space: $ROOT_SPACE"
 
-exit 0
+exit 
