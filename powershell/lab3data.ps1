9a0242df21c1655347b4c86133bc4e672eeb7d9a
Sukhman200516157 <124110347+Sukhman200516157@users.noreply.github.com>
2023-04-12 19:14:39 -0700

    Modules added

--- powershell/lab3data.ps1
+++ powershell/lab3data.ps1
@@ -1,14 +1,1 @@
-$adapters = Get-CimInstance Win32_NetworkAdapterConfiguration | Where-Object { $_.IPEnabled }
-
-$report = $adapters | Select-Object Description, Index, @{
-        Label = "IP Address"
-        Expression = { $_.IPAddress -join ", " }
-    }, @{
-        Label = "Subnet Mask"
-        Expression = { $_.IPSubnet -join ", " }
-    }, DNSDomain, @{
-        Label = "DNS Server"
-        Expression = { $_.DNSServerSearchOrder -join ", " }
-    }
-
-$report | Format-Table
+#script
